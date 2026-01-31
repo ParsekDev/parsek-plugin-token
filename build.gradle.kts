80df5c0e2cad618a7eae5718f1e6a96d7108ef95
kahverengi <ahmet@duruer.dev>
2026-01-31 19:42:11 +0300

    fix: fixed jwt dependency is scoped

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     compileOnly("io.vertx:vertx-jdbc-client:$vertxVersion")
 
     // https://mvnrepository.com/artifact/com.auth0/java-jwt
-    implementation("com.auth0:java-jwt:4.4.0")
+    api("com.auth0:java-jwt:4.4.0")
 
     // https://mvnrepository.com/artifact/com.google.code.gson/gson
     compileOnly("com.google.code.gson:gson:$gsonVersion")
