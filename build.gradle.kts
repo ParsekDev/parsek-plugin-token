517482bfc49fb4ef77fc68cb1fa490a6b74d623e
kahverengi <ahmet@duruer.dev>
2026-01-31 19:04:38 +0300

    feat: upgraded Kotlin version to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("kapt") version "2.0.21"
+    `java-library`
+    kotlin("jvm") version "2.3.0"
+    kotlin("kapt") version "2.3.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.jreleaser") version "1.14.0"
     `maven-publish`
