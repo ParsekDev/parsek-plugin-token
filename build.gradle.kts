ba71d6341ed6d3ff6522a81d69936fe41f3f47bf
kahverengi <ahmet@duruer.dev>
2026-03-03 23:28:59 +0300

    fix: fixed plugin jar is not copied

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,7 +104,7 @@ tasks {
 
     register("copyJar") {
         outputs.upToDateWhen { false }
-        mustRunAfter(shadowJar)
+        dependsOn(shadowJar)
 
         pluginsDir?.let {
             doLast {
